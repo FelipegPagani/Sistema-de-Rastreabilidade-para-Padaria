b0eb02abf1f33e68838992a5ecd6531649df33cb
FelipegPagani <fgeremiapagani@gmail.com>
2026-07-09 21:05:47 -0300

    Implementação método builder para clientes

--- src/main/java/padaria/model/Clientes.java
+++ src/main/java/padaria/model/Clientes.java
@@ -10,30 +10,19 @@ public class Clientes implements Serializable{
     private String cpf;
     private String nome;
 
-    public Clientes(String nome, String cpf, String endereco){
-        setCPF(cpf);
-        setNome(nome);
+    private Clientes(ClientesBuilder clientesBuilder){
+        this.nome = clientesBuilder.nome;
+        this.cpf = clientesBuilder.cpf;
     }
 
-    public void setCPF(String cpf) {
-        if(cpf.length() < 11 || cpf == null || cpf.trim().isEmpty()){
-            return;
-        }
-        this.cpf = cpf;    
+    public static ClientesBuilder builder(){
+        return new ClientesBuilder();
     }
 
     public String getCpf() {
         return cpf;
     }
-
-    public void setNome(String nome){
-        if(nome.isBlank() || nome.isEmpty()){
-            Video.mensagemErro("Nome Invalido.");
-            return;
-        }
-        this.nome = nome;
-    }
-
+    
     public String getNome(){
         return nome;
     }
@@ -41,5 +30,36 @@ public class Clientes implements Serializable{
     @Override
     public String toString() {
         return "Nome: " + nome + " | CPF: " + cpf;
+    }
+
+    public static class ClientesBuilder{
+        private String nome;
+        private String cpf;
+
+
+        public ClientesBuilder setNome(String nome){
+        if(nome.isBlank() || nome.trim().isEmpty()){
+           throw new IllegalArgumentException();
+        }
+        if(!nome.matches("[a-zA-ZÀ-ú\\s]+")){//valida que a string contenha apenas letras e espaços, sem números ou caracteres especiais
+            throw new NumberFormatException();
+        }
+
+            this.nome = nome;
+            return this;    
+        }
+
+        public ClientesBuilder setCPF(String cpf) {
+            if(cpf.length() < 11 || cpf == null || cpf.trim().isEmpty()){
+                throw new IllegalArgumentException();
+            }
+            this.cpf = cpf;
+            return this; 
+        }
+
+        public Clientes construir(){
+            return new Clientes(this);
+        }
+
     }
 }
